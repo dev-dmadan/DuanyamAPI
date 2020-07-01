5bfe8bffb99404144c7eca7af2097e75d330fd92
Romadan Saputra <42397721+dev-dmadan@users.noreply.github.com>
2020-07-01 10:56:57 +0700

    Modul Login
    > route login
    > migration dan seeder table users
    > controller login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use \Firebase\JWT\JWT;
+use Exception;
+
+class LoginController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function login(Request $request)
+    {
+        $result = (Object)array(
+            'success' => false,
+            'message' => null,
+            'token' => null
+        );
+        $status_code = 200;
+        $isError = true;
+
+        $userName = $request->has('UserName') ? $request->input('UserName') : null;
+        $userPassword = $request->has('UserPassword') ? $request->input('UserPassword') : null;
+
+        try {
+            if(!$userName || !$userPassword) {
+                $isError = false;
+                throw new Exception('Username atau Password anda salah');
+            }
+
+            $dataUser = DB::table('users')->where('username', $userName)->get();
+            if(empty($dataUser)) {
+                $isError = false;
+                throw new Exception('Username atau Password anda salah');
+            }
+
+            if(!Hash::check($userPassword, $dataUser[0]->password)) {
+                $isError = false;
+                throw new Exception('Username atau Password anda salah');
+            }
+
+            $result->token = $this->generateToken((Object)array(
+                'UserName' => $userName
+            ));
+            $result->success = true;
+        } catch (Exception $e) {
+            $status_code = $isError ? 400 : 200;
+            $result->message = $e->getMessage();
+        }
+
+        return response()->json($result, $status_code);
+    }
+
+    private function generateToken($data)
+    {
+        $payload = array(
+            "iss" => env('APP_URL'),
+            "aud" => env('APP_URL'),
+            "iat" => time(),
+            "nbf" => time(),
+            "exp" => time() + (int)env('JWT_EXP'),
+            "data" => $data
+        );
+        $jwt = JWT::encode($payload, env('JWT_KEY'));
+
+        return $jwt;
+    }    
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => '410006e1-ca4e-4502-a9ec-e54d922d',
+            'username' => 'Supervisor',
+            'password' => Hash::make('Supervisor'),
+        ]);
+    }
+}
