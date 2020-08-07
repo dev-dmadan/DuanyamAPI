d1cde5dccecb04043116e60429a62fffb8b539dd
Ahmad Bagus <50206121+ahmadbagus99@users.noreply.github.com>
2020-08-07 20:31:17 +0700

    Equalizer Loading

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,19 @@
         <div class="box box-custom">
 
             <div class="loader-wrapper">
-                <div class="loader is-loading"></div>
+                <!-- <div class="loader is-loading"></div> -->
+                <div class="Preloader">
+                    <div class="Preloader-squareContainer">
+                    <div class="Preloader-square"></div>
+                    <div class="Preloader-square"></div>
+                    <div class="Preloader-square"></div>
+                    <div class="Preloader-square"></div>
+                    <div class="Preloader-square"></div>
+                    <div class="Preloader-square"></div>
+                    </div>
+                    <!-- <div class="Preloader-message" style='font-family:"Bpmonline";'>Loading</div> -->
+                </div>
+
             </div>
 
             <div class="columns">
@@ -82,7 +94,9 @@
                 </div>
             </div>
         </div>
-        
+
+        <script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-157cd5b220a5c80d4ff8e0e70ac069bffd87a61252088146915e8726e5d9f147.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
         <script src="https://code.highcharts.com/highcharts.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/litepicker/dist/js/main.js"></script>
@@ -91,6 +105,7 @@
             const MAX_YEAR = @json(isset($maxYear) ? $maxYear : null, JSON_PRETTY_PRINT);
         </script>
         <script src="/js/app.js"></script>
+        <script src="/js/loadingScript.js"></script>
         
         @yield('custom-js')
 
